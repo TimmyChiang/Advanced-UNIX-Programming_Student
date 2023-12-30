c405bbc166dc27aef3c8ea8850629320e305fcce
TimmyChiang <timmychiang0506@gmail.com>
2023-12-30 20:25:59 +0800

    Add assignment8 solution

--- assignment8/assignment8.c
+++ assignment8/assignment8.c
@@ -6,114 +6,99 @@
 
 static volatile sig_atomic_t sigflag; /* set nonzero by sig handler */
 static sigset_t newmask, oldmask, zeromask;
+FILE* fp;
+pid_t pid;
 
-static void
-sig_usr(int signo)  /* one signal handler for SIGUSR1 and SIGUSR2 */
-{
-	sigflag = 1;
+static void sig_usr(int signo) { /* one signal handler for SIGUSR1 and SIGUSR2 */
+    sigflag = 1;
 }
 
-static void TELL_WAIT(void)
-{
-	if (signal(SIGUSR1, sig_usr) == SIG_ERR)
-		perror("signal(SIGUSR1) error");
+static void TELL_WAIT(void) {
+    if (signal(SIGUSR1, sig_usr) == SIG_ERR)
+        perror("signal(SIGUSR1) error");
 
-	if (signal(SIGUSR2, sig_usr) == SIG_ERR)
-		perror("signal(SIGUSR2) error");
+    if (signal(SIGUSR2, sig_usr) == SIG_ERR)
+        perror("signal(SIGUSR2) error");
 
-	sigemptyset(&zeromask);
-	sigemptyset(&newmask);
-	sigaddset(&newmask, SIGUSR1);
-	sigaddset(&newmask, SIGUSR2);
+    sigemptyset(&zeromask);
+    sigemptyset(&newmask);
+    sigaddset(&newmask, SIGUSR1);
+    sigaddset(&newmask, SIGUSR2);
 
-	/* Block SIGUSR1 and SIGUSR2, and save current signal mask */
-	if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
-		perror("SIG_BLOCK error");
+    /* Block SIGUSR1 and SIGUSR2, and save current signal mask */
+    if (sigprocmask(SIG_BLOCK, &newmask, &oldmask) < 0)
+        perror("SIG_BLOCK error");
 }
 
-static void TELL_PARENT(void)
-{
-	kill(getppid(), SIGUSR2); /* tell parent we’re done */
+static void TELL_PARENT(void) {
+    kill(getppid(), SIGUSR2); /* tell parent we’re done */
 }
 
-static void WAIT_PARENT(void)
-{
-	while (sigflag == 0)
-		sigsuspend(&zeromask);  /* and wait for parent */
-	sigflag = 0;
-	/* Reset signal mask to original value */
-	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
-		perror("SIG_SETMASK error");
+static void WAIT_PARENT(void) {
+    while (sigflag == 0)
+        sigsuspend(&zeromask); /* and wait for parent */
+    sigflag = 0;
+    /* Reset signal mask to original value */
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+        perror("SIG_SETMASK error");
 }
 
-static void TELL_CHILD(pid_t pid)
-{
+static void TELL_CHILD(pid_t pid) {
     kill(pid, SIGUSR1); /* tell child we’re done */
 }
 
-static void WAIT_CHILD(void)
-{
-	while (sigflag == 0)
-		sigsuspend(&zeromask);  /* and wait for child */
-	sigflag = 0;
-	/* Reset signal mask to original value */
-	if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
-		perror("SIG_SETMASK error");
+static void WAIT_CHILD(void) {
+    while (sigflag == 0)
+        sigsuspend(&zeromask); /* and wait for child */
+    sigflag = 0;
+    /* Reset signal mask to original value */
+    if (sigprocmask(SIG_SETMASK, &oldmask, NULL) < 0)
+        perror("SIG_SETMASK error");
 }
 
-static int increment_value(FILE* const file)
-{
-	int value = 0;
-
-	fseek(file, 0, SEEK_SET);
-	fread(&value, sizeof(value), 1, file);
-
-	++value;
-
-	fseek(file, 0, SEEK_SET);
-	fwrite(&value, sizeof(value), 1, file);
-	fflush(file);
-
-	return value;
+static int increment_counter(FILE *const file) {
+    /* TODO */
+    int val;
+    fscanf(file, "%d", &val);
+    ++val;
+    fseek(file, 0L, SEEK_SET);
+    fprintf(file, "%d", val);
+    fclose(fp);
+    return val;
 }
 
-int main(void)
-{
-	const int NUM_ITERATIONS = 50;
+int main(void) {
+    /* TODO */
+    fp = fopen("result.txt", "w");
+    fprintf(fp, "%d", 0);
+    fclose(fp);
 
-	FILE* const file = fopen("./data", "w+");
-	if (file == NULL) {
-		perror("fopen");
-		return 1;
-	}
+    pid = fork();
+    TELL_WAIT();
 
-	TELL_WAIT();
-	int i;
-	const pid_t pid = fork();
+    while(!pid) {
+        fp = fopen("result.txt", "r+");
+        int cur = increment_counter(fp);
+        if (cur > 100) {
+            TELL_PARENT();
+            break;
+        }
+        printf("Child incrementing, value: %d\n", cur);
+        TELL_PARENT();
+        WAIT_PARENT();
+    }
 
-	if (pid < 0) {
-		perror("fork");
-		return 1;
-	}
+    while(pid) {
+        WAIT_CHILD();
+        fp = fopen("result.txt", "r+");
+        int cur = increment_counter(fp);
+        if (cur > 100) {
+            TELL_CHILD(pid);
+            break;
+        }
+        printf("Parent incrementing, value: %d\n", cur);
+        TELL_CHILD(pid);
+    }
 
-	if (pid == 0) {
-		for (i = 0; i < NUM_ITERATIONS; ++i) {
-			printf("Child incrementing, value: %d\n", increment_value(file));
-
-			TELL_PARENT();
-			WAIT_PARENT();
-		}
-	} else {
-		for (i = 0; i < NUM_ITERATIONS; ++i) {
-			WAIT_CHILD();
-
-			printf("Parent incrementing, value: %d\n", increment_value(file));
-
-			TELL_CHILD(pid);
-		}
-	}
-
-	fclose(file);
-
-	return 0;
+    return 0;
 }
